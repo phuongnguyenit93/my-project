a6a4f24f966e309a786970d1ea5d5fcf92094f7c
phuong <phuongnguyenit93@gmail.com>
2023-04-13 10:54:23 +0700

    Log4J

---
+++ src/main/java/com/project/database/controller/Log4jController.java
@@ -0,0 +1,22 @@
+package com.project.database.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/log4j")
+public class Log4jController {
+    private static Logger log = LogManager.getLogger(Log4jController.class);
+
+    @GetMapping("/example")
+    public void log4jExample() {
+        log.debug("This is debug");
+        log.info("This is info");
+        log.warn("This is warn");
+        log.error("This is error");
+        log.fatal("This is trace");
+    }
+}
